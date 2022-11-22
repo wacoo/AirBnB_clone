26937c4ae963bfdf9136300a1d3d482093dabb53
wac <wabaham9@gmail.com>
2022-11-22 12:48:04 +0000

    q5...

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 
 import uuid
 from datetime import datetime
+import storage
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs and len(kwargs) != 0:
@@ -23,11 +25,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[" + self.__class__.__name__ +"] (" + str(self.id) + ") " + str(self.__dict__)
 
     def save(self):
+        storage.save(self)
         self.updated_at = datetime.now()
     def to_dict(self):
         self.__dict__["__class__"] = str(self.__class__.__name__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # Todo
+import json
+import sys
 
+sys.path.append('../models')
+from models.base_model import to_dict
 class FileStorage:
     def __init__(self, filepath, objects):
         self.__filepath = filepath
@@ -8,6 +12,11 @@ class FileStorage:
     def all(self):
         return self.__objects
     def new(self, obj):
-        self.__objects = obj
+        self.__objects[self.__class__.__name__.id] = obj
     def save(self):
-
+        with open("file.json", "w") as jfile:
+            if self.__objects is None:
+                jfile.write("[]")
+            else:
+                list_of_dicts = [d.to_dict() for d in self.__objects]
+                jfile.write(json.dumps(list_of_dicts))
